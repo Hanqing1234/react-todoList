779abcf0b472a2d2272851db6f7c851561f252a5
Hanqing Liu <lhq3361951@gmail.com>
2021-12-28 17:00:01 -0500

    used css module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
-import styled from 'styled-components'
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
+// const FormControl = styled.div`
  
-  margin: 0.5rem 0;
+//   margin: 0.5rem 0;
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc;
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
-&.invalid input{
-  border-color: red;
-  background: #ffd7d7;
-}
+// &.invalid input{
+//   border-color: red;
+//   background: #ffd7d7;
+// }
 
-&.invalid label{
-  color: red
-}`
+// &.invalid label{
+//   color: red
+// }`
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -61,13 +60,13 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'invalid'}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
